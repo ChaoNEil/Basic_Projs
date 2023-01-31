775dd482b18d2110e7dd470b5da0b389bacd7353
ChaoNEil <62066462+ChaoNEil@users.noreply.github.com>
2023-01-31 12:15:00 +0530

    like a digital clock: added sec,min,hr

--- Countdown timer.py
+++ Countdown timer.py
@@ -3,7 +3,8 @@ import time
 my_time = int(input("Enter the time in secs: "))
 
 for i in range(my_time,0,-1):
-    print(i)
+    seconds = i % 60
+    print(f"00:00:{seconds}")
     time.sleep(1)
 
-print("Time's Up")
+print("Time's Up.")
