66975b1a9e578ce4a21440a4df668c809f55af3e
ChaoNEil <62066462+ChaoNEil@users.noreply.github.com>
2023-02-01 10:53:51 +0530

    idk why i am being forced to commit this

--- Countdown timer.py
+++ Countdown timer.py
@@ -4,7 +4,9 @@ my_time = int(input("Enter the time in secs: "))
 
 for i in range(my_time,0,-1):
     seconds = i % 60
-    print(f"00:00:{seconds}")
+    mins = i // 60
+    hours = i // 3600 #there are 3600 seconds in a hour
+    print(f"{hours:02}:{mins:02}:{seconds:02}")
     time.sleep(1)
 
 print("Time's Up.")
