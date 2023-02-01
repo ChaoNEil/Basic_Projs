9b6f6f1f978071ba6409e7d77bc3fbfb4f51cad1
ChaoNEil <62066462+ChaoNEil@users.noreply.github.com>
2023-02-01 11:18:23 +0530

    fixed bugs, and added "Encryption" code

--- Encryption.py
+++ Encryption.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-chars = string.punctuation + string.digits + string.ascii_letters
+chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
 
@@ -9,3 +9,14 @@ random.shuffle(key)
 
 print("chars: " , chars)
 print("key:" , key)
+
+#Enryption
+text = input("Enter your message to encrypt: ")
+chipher_text = ""
+
+for letter in text:
+    index = chars.index(letter)
+    chipher_text += key[index]
+
+print("Original message: ", text)
+print("Encrypted message: ", chipher_text)
